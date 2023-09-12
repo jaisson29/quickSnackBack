2b513cd6f9a20b5ba9970cba1cf075336dd00a4c
JaissonV <jaissonvalbuena29@outlook.com>
2023-09-11 23:17:50 -0500

    .

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -5,7 +5,7 @@ class UserModel {
     return new Promise((resolve, reject) => {
       try {
         const query =
-          'SELECT usu.usuId, usu.usuTipoDoc, usu.usuNoDoc, usu.usuGen, usu.usuNom, usu.usuEmail, usu.usuContra, usu.usuIngreso, usu.usuImg, per.perfilNom, usu.usuFecha, usu.usuPassCode ' +
+          'SELECT usu.usuId, usu.usuTipoDoc, usu.usuNoDoc , usu.usuGen, usu.usuNom, usu.usuEmail, usu.usuContra, usu.usuIngreso, usu.usuImg, per.perfilNom, usu.usuFecha, usu.usuPassCode ' +
           'FROM usuario AS usu ' +
           'INNER JOIN perfil AS per ' +
           'ON usu.perfilId = per.perfilId ';
@@ -33,26 +33,7 @@ class UserModel {
           'ON usu.perfilId = per.perfilId ' +
           'WHERE usu.usuId = ? ';
         const { usuId } = data;
-      try {
-        const sql =
-          'SELECT usu.usuId, usu.usuTipoDoc, usu.usuGen, usu.usuNom, usu.usuEmail, usu.usuContra, usu.usuIngreso, usu.usuImg, per.perfilNom, usu.usuFecha, usu.usuPassCode ' +
-          'FROM usuario AS usu ' +
-          'INNER JOIN perfil AS per ' +
-          'ON usu.perfilId = per.perfilId ' +
-          'WHERE usu.usuId = ? ';
-        const { usuId } = data;
 
-        db.query(sql, [usuId], (err, result) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(result);
-          }
-        });
-      } catch (err) {
-        console.log(err);
-        reject(err);
-      }
         db.query(sql, [usuId], (err, result) => {
           if (err) {
             reject(err);
@@ -69,25 +50,6 @@ class UserModel {
 
   static getOneXEmailXContra(data) {
     return new Promise((resolve, reject) => {
-      try {
-        const sql =
-          'SELECT usu.usuId, usu.usuTipoDoc, usu.usuGen, usu.usuNom, usu.usuEmail, usu.usuContra, usu.usuIngreso, usu.usuImg, per.perfilNom, per.perfilId, usu.usuFecha, usu.usuPassCode ' +
-          'FROM usuario AS usu ' +
-          'INNER JOIN perfil AS per ' +
-          'ON usu.perfilId = per.perfilId ' +
-          'WHERE usuEmail = ? AND usuContra = ?';
-        const { usuEmail, usuContra } = data;
-        db.query(sql, [usuEmail, usuContra], (err, result) => {
-          if (err) {
-            reject(err);
-          } else {
-            resolve(result);
-          }
-        });
-      } catch (err) {
-        console.log(err);
-        reject(err);
-      }
       try {
         const sql =
           'SELECT usu.usuId, usu.usuTipoDoc, usu.usuGen, usu.usuNom, usu.usuEmail, usu.usuContra, usu.usuIngreso, usu.usuImg, per.perfilNom, per.perfilId, usu.usuFecha, usu.usuPassCode ' +
@@ -150,44 +112,6 @@ class UserModel {
         console.log('error', err);
         reject(err);
       }
-      try {
-        const query =
-          'INSERT INTO usuario(usuTipoDoc, usuNoDoc, usuGen, usuNom, usuEmail, usuContra, usuIngreso, perfilId) ' +
-          'VALUES (?, ?, ?, ?, ?, ?, ?, ?)';
-        const {
-          usuTipoDoc,
-          usuNoDoc,
-          usuGen,
-          usuNom,
-          usuEmail,
-          usuContra,
-          usuIngreso,
-          perfilId,
-        } = data;
-        db.query(
-          query,
-          [
-            usuTipoDoc,
-            usuNoDoc,
-            usuGen,
-            usuNom,
-            usuEmail,
-            usuContra,
-            usuIngreso,
-            perfilId,
-          ],
-          (err, result) => {
-            if (result && result.affectedRows === 1) {
-              resolve(result);
-            } else {
-              reject(err);
-            }
-          }
-        );
-      } catch (err) {
-        console.log('error', err);
-        reject(err);
-      }
     });
   }
 
@@ -206,68 +130,7 @@ class UserModel {
           usuEmail,
           usuContra,
           usuIngreso,
-      try {
-        const query =
-          'UPDATE usuario ' +
-          'SET usuTipoDoc = ?, usuNoDoc = ?, usuGen = ?, usuNom = ?, usuEmail = ?, usuContra = ?, usuIngreso = ?, perfilId = ? ' +
-          'WHERE usuId = ?';
-        const {
-          usuTipoDoc,
-          usuNoDoc,
-          usuGen,
-          usuNom,
-          usuEmail,
-          usuContra,
-          usuIngreso,
 
-          perfilId,
-          usuId,
-        } = data;
-        db.query(
-          query,
-          [
-            usuTipoDoc,
-            usuNoDoc,
-            usuGen,
-            usuNom,
-            usuEmail,
-            usuContra,
-            usuIngreso,
-            perfilId,
-            usuId,
-          ],
-          (err, result) => {
-            if (result && result.affectedRows === 1) {
-              resolve(result);
-            } else {
-              reject(err);
-            }
-          }
-        );
-      } catch (err) {
-        console.log('Error en la consulta', err);
-        reject(err);
-      }
-    });
-  }
-
-  static delete(data) {
-    return new Promise((resolve, reject) => {
-      try {
-        const sql = 'DELETE FROM usuario WHERE usuId = ?';
-        const { usuId } = data;
-
-        db.query(sql, [usuId], (err, result) => {
-          if (result && result.affectedRows === 1) {
-            resolve(result);
-          } else {
-            reject(err);
-          }
-        });
-      } catch (err) {
-        console.log('error', err);
-        reject(err);
-      }
           perfilId,
           usuId,
         } = data;
