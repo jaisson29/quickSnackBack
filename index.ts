002c6b5861fecbef88d1d2f3e886291383ff3e34
Jayval <jaissonvalbuena29@outlook.com>
2024-02-02 12:03:18 -0500

    comment the console.log of the server status

--- index.ts
+++ index.ts
@@ -4,4 +4,4 @@ import app from './src/app';
 app.set('port', process.env.PORT);
 
 app.listen(app.get('port'));
-console.log(`server run in http://localhost:${app.get('port')}`)
+// console.log(`server run in http://localhost:${app.get('port')}`)
