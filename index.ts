e66bcfc7ca7fd5476954b9d7752ed7732ad18d41
Jayval <jaissonvalbuena29@outlook.com>
2024-02-02 11:13:01 -0500

    changing the env variable from APP_PROT to PORT

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import 'dotenv/config'
 import app from './src/app'
 
 
-app.set('port', process.env.APP_PORT)
+app.set('port', process.env.PORT)
 
 app.listen(app.get('port'))
 console.log(`server run in http://localhost:${app.get('port')}`)
